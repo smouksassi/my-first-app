59389d3ea84bfe2b68fb525750a19882726acef5
Your Name <you@example.com>
2026-03-12 06:33:37 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "my-first-app",
+  base: "/my-first-app/",
   plugins: [react()],
 });
